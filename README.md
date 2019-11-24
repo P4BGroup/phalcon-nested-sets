802ec53699fa290d953ccca6f0f1e1fcac7fc9dd
Flaviu Chelaru <flaviu.chelaru@p4b.ro>
2019-11-24 15:48:23 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# phalcon-nested-sets
-Nested Set Model implementation for Phalcon Framework
+Nested Set Behaviour
+--- 
+
+Phalcon implementation for tree / hierarchy through nested sets implementation.
+It will calculate edges and depth for a category on creation, update and delete.
+
+Moving "branches" is not supported (o sub-node with all it's sub-nodes) and won't be. This tool is meant to "react" to changes on a single record and keep the rest of the tree position in place.
+
+prerequisites
+---
+Your DB must have parent, left, right, depth columns. 
+Example:
+
+```SQL
+CREATE TABLE `categories` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`name` CHAR(50) NULL DEFAULT NULL,
+	`parent_id` INT(11) NULL DEFAULT NULL,
+	`_left` INT(11) NULL DEFAULT NULL,
+	`_right` INT(11) NULL DEFAULT NULL,
+	`_depth` INT(11) NULL DEFAULT NULL,
+	PRIMARY KEY (`id`)
+);
+```
+
+usage
+---
+
+```php
+
+class MyModel extends \Phalcon\Mvc\Model {
+    public function initialize() {
+        $this->addBehaviour(new \P4BGroup\NestedSets\Behaviour());
+    }
+}
+
+```
+
+references
+---
+* https://en.wikipedia.org/wiki/Nested_set_model
+* https://explainextended.com/2009/09/29/adjacency-list-vs-nested-sets-mysql/
+* https://medium.com/@Sumurai8/nested-sets-performant-attribute-calculation-on-collections-10289a30c0ab
+
+usage
+---
+this behaviour will automatically calculate the edges and depth of each node on save / delete
+
+Similar implementations on other frameworks
+---
+* https://github.com/etrepat/baum - laravel 
+* https://github.com/lazychaser/laravel-nestedset - laravel 
+* https://github.com/blt04/doctrine2-nestedset - doctrine
+* https://github.com/bartko-s/stefano-tree - zend / pdo
+* https://github.com/creocoder/yii2-nested-sets - yii
---
+++ src/Behavior.php
@@ -0,0 +1,276 @@
+<?php
+
+namespace P4BGroup\NestedSets;
+
+use Phalcon\Mvc\Model\Behavior as PhalconBehavior;
+use Phalcon\Mvc\Model\MetaDataInterface;
+use Phalcon\Mvc\ModelInterface;
+
+class Behavior extends PhalconBehavior
+{
+    /**
+     * @var string
+     */
+    private static $primaryKey = 'id';
+    /**
+     * @var string
+     */
+    private static $leftKey = 'lft';
+    /**
+     * @var string
+     */
+    private static $rightKey = 'rgt';
+    /**
+     * @var string
+     */
+    private static $depthKey = 'depth';
+    /**
+     * @var string
+     */
+    private static $parentKey = 'parent_id';
+    /**
+     * @var string
+     */
+    private static $primaryDbColumn;
+    /**
+     * @var string
+     */
+    private static $parentDbColumn;
+    /**
+     * @var string
+     */
+    private static $leftDbColumn;
+    /**
+     * @var string
+     */
+    private static $rightDbColumn;
+    /**
+     * @var string
+     */
+    private static $depthDbColumn;
+
+    /**
+     * @param string $type
+     * @param ModelInterface $model
+     * @return void
+     */
+    public function notify($type, ModelInterface $model): void
+    {
+        $options = $this->getOptions();
+        self::$primaryKey = $options['primaryKey'] ?? self::$primaryKey;
+        self::$leftKey = $options['leftKey'] ?? self::$leftKey;
+        self::$rightKey = $options['rightKey'] ?? self::$rightKey;
+        self::$depthKey = $options['depthKey'] ?? self::$depthKey;
+        self::$parentKey = $options['parentKey'] ?? self::$parentKey;
+
+        /** @var MetaDataInterface $metaData */
+        $metaData = $model->getModelsMetadata();
+        $columnMap = $metaData->getReverseColumnMap($model);
+
+        self::$primaryDbColumn = $columnMap[self::$primaryKey] ?? self::$primaryKey;
+        self::$parentDbColumn = $columnMap[self::$parentKey] ?? self::$parentKey;
+        self::$leftDbColumn = $columnMap[self::$leftKey] ?? self::$leftKey;
+        self::$rightDbColumn = $columnMap[self::$rightKey] ?? self::$rightKey;
+        self::$depthDbColumn = $columnMap[self::$depthKey] ?? self::$depthKey;
+
+        switch ($type) {
+            case 'beforeCreate':
+                $this->processCreateNodeWithParent($model);
+                break;
+            case 'beforeUpdate':
+                $this->processUpdate($model);
+                break;
+            case 'beforeDelete':
+                $this->processDelete($model);
+                break;
+        }
+    }
+
+    /**
+     * @param ModelInterface $model
+     */
+    public function processCreateRootNode(ModelInterface $model): void
+    {
+        $max = $model::maximum(['column' => self::$rightKey]);
+        $data = [
+            self::$leftKey => $max + 1,
+            self::$rightKey => $max + 2,
+            self::$depthKey => 0,
+        ];
+        $model->assign($data);
+    }
+
+    /**
+     * @param ModelInterface $model
+     */
+    public function processCreateNodeWithParent(ModelInterface $model): void
+    {
+        $parent = $model::findFirst([
+            self::$primaryKey . ' = :parent:',
+            'bind' => [
+                'parent' => $model->readAttribute(self::$parentKey)
+            ]
+        ]);
+
+        if (!$parent) {
+            $this->processCreateRootNode($model);
+            return;
+        }
+
+        $query = 'UPDATE `' . $model->getSource() . '` SET ' .
+            '`' . self::$rightDbColumn . '` = `' . self::$rightDbColumn . '` + 2 ' .
+            'WHERE `' . self::$rightDbColumn . '` >= :right';
+        $model->getWriteConnection()->query($query, [
+            'right' => $parent->readAttribute(self::$rightKey)
+        ]);
+
+        $query = 'UPDATE `' . $model->getSource() . '` SET ' .
+            '`' . self::$leftDbColumn . '` = `' . self::$leftDbColumn . '` + 2 ' .
+            'WHERE `' . self::$leftDbColumn . '` >= :right';
+        $model->getWriteConnection()->query($query, [
+            'right' => $parent->readAttribute(self::$rightKey)
+        ]);
+
+        $model->assign([
+            self::$leftKey => $parent->readAttribute(self::$rightKey),
+            self::$rightKey => $parent->readAttribute(self::$rightKey) + 1,
+            self::$depthKey => $parent->readAttribute(self::$depthKey) + 1,
+        ]);
+    }
+
+    /**
+     * @param ModelInterface $model
+     */
+    public function processDelete(ModelInterface $model): void
+    {
+        $parent = $model::findFirst([
+            self::$primaryKey . ' = :parent:',
+            'bind' => [
+                'parent' => $model->readAttribute(self::$parentKey)
+            ]
+        ]);
+
+        $query = 'UPDATE `' . $model->getSource() . '` SET ' .
+            '`' . self::$rightDbColumn . '` = `' . self::$rightDbColumn . '` - 1, ' .
+            '`' . self::$leftDbColumn . '` = `' . self::$leftDbColumn . '` - 1, ' .
+            '`' . self::$depthDbColumn . '` = `' . self::$depthDbColumn . '` - 1, ' .
+            '`' . self::$parentDbColumn . '` = :parent ' .
+            'WHERE `' . self::$leftDbColumn . '` > :left AND `' . self::$rightDbColumn . '` < :right';
+
+        $model->getWriteConnection()->query($query, [
+            'right' => $model->readAttribute(self::$rightKey),
+            'left' => $model->readAttribute(self::$leftKey),
+            'parent' => $parent ? $parent->readAttribute(self::$parentKey) : 0
+        ]);
+
+        $query = 'UPDATE `' . $model->getSource() . '` SET ' .
+            '`' . self::$rightDbColumn . '` = `' . self::$rightDbColumn . '` - 2 ' .
+            'WHERE `' . self::$rightDbColumn . '` > :right';
+        $model->getWriteConnection()->query($query, [
+            'right' => $model->readAttribute(self::$rightKey)
+        ]);
+
+        $query = 'UPDATE `' . $model->getSource() . '` SET' .
+            ' `' . self::$leftDbColumn . '` = `' . self::$leftDbColumn . '` - 2 ' .
+            'WHERE `' . self::$leftDbColumn . '` > :right';
+        $model->getWriteConnection()->query($query, [
+            'right' => $model->readAttribute(self::$rightKey)
+        ]);
+    }
+
+    public function processUpdate(ModelInterface $model): void
+    {
+        $currentModel = $model::findFirst([
+            self::$primaryKey . ' = :id:',
+            'bind' => [
+                'id' => $model->readAttribute(self::$primaryKey)
+            ]
+        ]);
+        $parentModel = $model::findFirst([
+            self::$primaryKey . ' = :id:',
+            'bind' => [
+                'id' => $model->readAttribute(self::$parentKey)
+            ]
+        ]);
+
+        // parent has changed (unset parent or set a new parent)
+        if ($model->readAttribute(self::$parentKey) == $currentModel->readAttribute(self::$parentKey)) {
+            return;
+        }
+
+        $maxRight = $model::maximum(['column' => self::$rightKey]);
+
+        // upgrade children of current node in the tree
+        $query = 'UPDATE `' . $model->getSource() . '` SET ' .
+            '`' . self::$depthDbColumn . '` = `' . self::$depthDbColumn . '` - 1, ' .
+            '`' . self::$rightDbColumn . '` = `' . self::$rightDbColumn . '` - 1, ' .
+            '`' . self::$leftDbColumn . '` = `' . self::$leftDbColumn . '` - 1, ' .
+            '`' . self::$parentDbColumn . '` = :parentID ' .
+            'WHERE `' . self::$leftDbColumn . '` > :left AND `' . self::$rightDbColumn . '` < :right ' .
+            'AND `' . self::$primaryDbColumn . '` != :id';
+        $model->getWriteConnection()->query($query, [
+            'right' => $currentModel->readAttribute(self::$rightKey),
+            'left' => $currentModel->readAttribute(self::$leftKey),
+            'parentID' => $currentModel->readAttribute(self::$parentKey),
+            'id' => $model->readAttribute(self::$primaryKey),
+        ]);
+        // update nodes on the right of the tree
+        $query = 'UPDATE `' . $model->getSource() . '` SET ' .
+            '`' . self::$rightDbColumn . '` = `' . self::$rightDbColumn . '` - 2 ' .
+            'WHERE `' . self::$rightDbColumn . '` > :right AND `' . self::$primaryDbColumn . '` != :id';
+        $model->getWriteConnection()->query($query, [
+            'right' => $currentModel->readAttribute(self::$rightKey),
+            'id' => $model->readAttribute(self::$primaryKey),
+        ]);
+
+        $query = 'UPDATE `' . $model->getSource() . '` SET ' .
+            '`' . self::$leftDbColumn . '` = `' . self::$leftDbColumn . '` - 2 ' .
+            'WHERE `' . self::$leftDbColumn . '` > :right AND `' . self::$primaryDbColumn . '` != :id';
+        $model->getWriteConnection()->query($query, [
+            'right' => $currentModel->readAttribute(self::$rightKey),
+            'id' => $model->readAttribute(self::$primaryKey),
+        ]);
+
+        // unset the parent - make the node a root
+        if (!$parentModel) {
+            $model->assign([
+                self::$leftKey => $maxRight + 1,
+                self::$rightKey => $maxRight + 2,
+                self::$depthKey => 0,
+                self::$parentKey => 0,
+            ]);
+
+            return;
+        }
+
+        $right = $parentModel->readAttribute(self::$rightKey);
+        if ($parentModel->readAttribute(self::$rightKey) > $currentModel->readAttribute(self::$rightKey)
+            && $parentModel->readAttribute(self::$rightKey) > $currentModel->readAttribute(self::$leftKey)) {
+            $right = $parentModel->readAttribute(self::$rightKey) - 2;
+        } elseif ($parentModel->readAttribute(self::$rightKey) < $currentModel->readAttribute(self::$rightKey)
+            && $parentModel->readAttribute(self::$rightKey) > $currentModel->readAttribute(self::$leftKey)) {
+            $right = $parentModel->readAttribute(self::$rightKey) - 1;
+        }
+
+        $query = 'UPDATE `' . $model->getSource() . '` SET ' .
+            '`' . self::$rightDbColumn . '` = `' . self::$rightDbColumn . '` + 2 ' .
+            'WHERE `' . self::$rightDbColumn . '` >= :right';
+        $model->getWriteConnection()->query($query, [
+            'right' => $right,
+        ]);
+
+        $query = 'UPDATE `' . $model->getSource() . '` SET ' .
+            '`' . self::$leftDbColumn . '` = `' . self::$leftDbColumn . '` + 2 ' .
+            'WHERE `' . self::$leftDbColumn . '` >= :right';
+        $model->getWriteConnection()->query($query, [
+            'right' => $right,
+        ]);
+
+        $model->assign([
+            self::$leftKey => $right,
+            self::$rightKey => $right + 1,
+            self::$depthKey => $parentModel->readAttribute(self::$depthKey) + 1,
+            self::$parentKey => 0,
+        ]);
+    }
+}
