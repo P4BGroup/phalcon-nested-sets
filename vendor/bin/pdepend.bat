e2537c7592fe5e93fbd90979f2c535cfc4002287
Flaviu Chelaru <flaviu.chelaru@p4b.ro>
2019-11-24 15:49:25 +0200

    removed vendor and composer.lock

--- vendor/bin/pdepend.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-setlocal DISABLEDELAYEDEXPANSION
-SET BIN_TARGET=%~dp0/../pdepend/pdepend/src/bin/pdepend
-php "%BIN_TARGET%" %*
--- vendor/bin/phpmd.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-setlocal DISABLEDELAYEDEXPANSION
-SET BIN_TARGET=%~dp0/../phpmd/phpmd/src/bin/phpmd
-php "%BIN_TARGET%" %*
